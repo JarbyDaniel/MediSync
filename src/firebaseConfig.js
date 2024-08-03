6f9550bb25245be60825ce5fdd18ac8096b2add9
JarbyDaniel <dsalazarg@unicauca.edu.co>
2024-08-03 09:23:05 -0500

    remove base

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -3,12 +3,7 @@ import { getFirestore } from "firebase/firestore";
 
 // Tu configuración de Firebase
 const firebaseConfig = {
-    apiKey: "AIzaSyA4y-7HsycFt8x9aEvF8Ml0wSzc9Mxehus",
-    authDomain: "techx-48c9d.firebaseapp.com",
-    projectId: "techx-48c9d",
-    storageBucket: "techx-48c9d.appspot.com",
-    messagingSenderId: "345860801548",
-    appId: "1:345860801548:web:210b6612e39451f7aa9a29"
+    
 };
 
 // Inicializa Firebase
